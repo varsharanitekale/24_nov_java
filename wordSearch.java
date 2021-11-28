4ad05a146fd637ae03a2a4633af5b5631dedf24e
varsharanitekale <varsharanitekale50@gmail.com>
2021-11-28 20:53:05 +0530

    inital

---
+++ wordSearch.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class wordSearch {
+    //search a word inside a string
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Sentence : "); 
+		String str=sc.nextLine();
+        System.out.println("Enter the word to be searched :");
+		String search=sc.nextLine();
+		int intIndex = str.indexOf(search);
+		if(intIndex == - 1) {
+			System.out.println("word not found");
+		} else {
+			System.out.println("word Found at index "+ intIndex);
+		}
+        sc.close();
+	}
+  
+}
+    
+}
